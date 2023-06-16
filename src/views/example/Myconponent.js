8f837edca5a634f7c062cb71b07f8bde21dedb49
tighostle <hachac2k2@gmail.com>
2023-06-16 18:54:19 +0700

    #4 - DOM events

--- src/views/example/Myconponent.js
+++ src/views/example/Myconponent.js
@@ -15,22 +15,39 @@ class MyComponent extends React.Component {
     <React.Fragment> </React.fragment> === <> </>
 
     */
+    handleOnChangeName = (event) => {
+        this.setState(
+            {
+                name: event.target.value
+            }
+        )
+    }
+
+    handleClickButton = () => {
+        console.log('hit the button')
+        alert('Click me')
+    }
+
     render() {
 
         return (
             <>
                 <div className='first'>
-                    hello my component. My name is {this.state['name']}
+                    <input value={this.state.name}
+                        type='text' onChange={(event) => this.handleOnChangeName(event)} />
+                    My name is {this.state['name']}
                 </div>
 
                 <div className='second'>
                     my age: {this.state.age}
                 </div>
+                <div className='third'>
+                    <button onClick={() => { this.handleClickButton() }}>Click me</button>
+                </div>
             </>
         )
 
     }
-
 }
 
 export default MyComponent;
