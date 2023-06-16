2a3b326a20ca7cdc15abbfd7a2636ac81c932b47
tighostle <hachac2k2@gmail.com>
2023-06-16 15:56:40 +0700

    #3 - state

--- src/views/example/Myconponent.js
+++ src/views/example/Myconponent.js
@@ -2,9 +2,31 @@
 import React from 'react'
 
 class MyComponent extends React.Component {
+
+    // key:value
+    state = {
+        name: 'Hac',
+        age: '21'
+    }
+
+    /* 
+    JSX => return block
+
+    <React.Fragment> </React.fragment> === <> </>
+
+    */
     render() {
+
         return (
-            <div>hello my component Hac</div>
+            <>
+                <div className='first'>
+                    hello my component. My name is {this.state['name']}
+                </div>
+
+                <div className='second'>
+                    my age: {this.state.age}
+                </div>
+            </>
         )
 
     }
